73374b58f56ef4e04414504c63ecf526f215372d
Tl-lvl-lL <marianotorresleyva@gmail.com>
2021-12-20 14:19:59 -0500

    new assests, new performance

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img
+      @mouseenter="display"
+      @mouseleave="display"
+      alt="Vue logo"
+      src="./assets/logo.png"
+    />
+    <header-menu>
+      <template v-slot:header>
+        <h2>Here might be a page title</h2>
+      </template>
+    </header-menu>
+    <HelloWorld v-show="showUp" msg="Hello world" />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import HeaderMenu from "./components/HeaderNav.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    HeaderMenu,
+  },
+  data: function () {
+    return {
+      showUp: false,
+    };
+  },
+  methods: {
+    display() {
+      this.showUp = !this.showUp;
+    },
   },
 };
 </script>
---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Look at me</h1>
+    <slot name="header"></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "HeaderMenu",
+};
+</script>
+<style scoped></style>
